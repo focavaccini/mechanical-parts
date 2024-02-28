a7cdd0f81093ebfcee82cd4f98fb87c3d289d308
Fernando Cavaccini <fernandocavaccini@gmail.com>
2024-02-28 19:43:23 -0300

    Correção service

--- src/main/java/br/com/inventory/mechanicalparts/services/ProductImageService.java
+++ src/main/java/br/com/inventory/mechanicalparts/services/ProductImageService.java
@@ -1,12 +1,11 @@
 package br.com.inventory.mechanicalparts.services;
 
 import br.com.inventory.mechanicalparts.controllers.interfaces.IAbstractService;
-import br.com.inventory.mechanicalparts.entities.Address;
 import br.com.inventory.mechanicalparts.entities.ProductImages;
 
 import java.util.List;
 
-public interface ProductImageService extends IAbstractService<Address, Long> {
+public interface ProductImageService extends IAbstractService<ProductImages, Long> {
 
     ProductImages insert(ProductImages productImages);
 
