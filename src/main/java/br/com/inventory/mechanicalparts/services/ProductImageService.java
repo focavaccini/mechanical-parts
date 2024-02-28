9eca28ecf5a07a37e5eb128e17108ac5bab4d1ef
Fernando Cavaccini <fernandocavaccini@gmail.com>
2024-02-28 19:40:22 -0300

    Criado Service de ProductImages

---
+++ src/main/java/br/com/inventory/mechanicalparts/services/ProductImageService.java
@@ -0,0 +1,19 @@
+package br.com.inventory.mechanicalparts.services;
+
+import br.com.inventory.mechanicalparts.controllers.interfaces.IAbstractService;
+import br.com.inventory.mechanicalparts.entities.Address;
+import br.com.inventory.mechanicalparts.entities.ProductImages;
+
+import java.util.List;
+
+public interface ProductImageService extends IAbstractService<Address, Long> {
+
+    ProductImages insert(ProductImages productImages);
+
+    void update(Long idProductImages, ProductImages productImages);
+
+    ProductImages getById(Long idProductImages);
+
+    List<ProductImages> getAll();
+
+}
