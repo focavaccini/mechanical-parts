f3ec61c38d2445fc40f58af8c24dbb06f6c855f4
Fernando Cavaccini <fernandocavaccini@gmail.com>
2024-12-17 17:30:30 -0300

    Adicionado configuração de cache para o endpoint ProfessionalController

--- src/main/java/br/com/inventory/mechanicalparts/controllers/ProfessionalController.java
+++ src/main/java/br/com/inventory/mechanicalparts/controllers/ProfessionalController.java
@@ -5,6 +5,8 @@ import br.com.inventory.mechanicalparts.dtos.ProfessionalDTO;
 import br.com.inventory.mechanicalparts.entities.Professional;
 import br.com.inventory.mechanicalparts.services.ProfessionalService;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,27 +19,32 @@ public class ProfessionalController extends AbstractController<ProfessionalServi
 
     private ProfessionalService professionalService;
 
-    public Professional insert(@RequestBody ProfessionalDTO professionalDTO){
+    @CacheEvict(value = "professional", allEntries = true)
+    public Professional insert(@RequestBody ProfessionalDTO professionalDTO) {
         Professional professional = convert(professionalDTO, Professional.class);
         return professionalService.insert(professional);
     }
 
-    public void update(@PathVariable Long idProfessional, @RequestBody ProfessionalDTO professionalDTO){
+    @CacheEvict(value = "professional", allEntries = true)
+    public void update(@PathVariable Long idProfessional, @RequestBody ProfessionalDTO professionalDTO) {
         Professional professional = convert(professionalDTO, Professional.class);
         professionalService.update(idProfessional, professional);
     }
 
     @Override
+    @Cacheable(value = "professional")
     public List<ProfessionalDTO> getAll() {
         return convert(professionalService.getAll(), ProfessionalDTO.class);
     }
 
     @Override
+    @Cacheable(value = "professional", key = "#idProfessional")
     public ProfessionalDTO getById(Long idProfessional) {
-        return convert(professionalService.getById(idProfessional), ProfessionalDTO.class );
+        return convert(professionalService.getById(idProfessional), ProfessionalDTO.class);
     }
 
     @Override
+    @Cacheable(value = "professional", key = "#name")
     public ProfessionalDTO findByName(String name) {
         return convert(professionalService.findByName(name), ProfessionalDTO.class);
     }
